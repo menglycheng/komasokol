27faf6469bad12c255ea55d1bff6c3e028760165
mengly <mengly@menglys-MacBook-Pro.local>
2024-01-20 19:55:14 +0700

    add docker file

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+# Use the official Python image as the base image
+FROM python:3.8
+
+# Set the working directory in the container
+WORKDIR /app
+
+# Upgrade pip
+RUN pip install --no-cache-dir --upgrade pip
+
+# Copy the requirements.txt and install dependencies
+COPY requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copy the script and any other necessary files
+COPY script.py .
+
+# Expose the port your script is listening on (if applicable)
+EXPOSE 80
+
+# Define the command to run your script
+CMD ["python", "script.py"]
--- script.py
+++ script.py
@@ -1,98 +1,105 @@
 import os
-import telebot 
+import telebot
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
-import requests
 from dotenv import load_dotenv
+import requests
+
 load_dotenv()
 
-API =os.getenv('BOT_TOKEN')
-# Replace YOUR_TOKEN with your bot token obtained from BotFather
+API = os.getenv('BOT_TOKEN')
+URL = os.getenv('URL')
+
 bot = telebot.TeleBot(API)
 
-# create button for user to click
-connect_keyboard = InlineKeyboardMarkup()
-disconnect_keyboard = InlineKeyboardMarkup()
+def create_main_keyboard(chat_id):
+    keyboard = InlineKeyboardMarkup()
 
-service = InlineKeyboardButton('សេវាកម្ម', callback_data='service')
-contact = InlineKeyboardButton('លេខទំនាក់ទំនង', callback_data='contact')
-about = InlineKeyboardButton('អំពីយើង', callback_data='about')
-location = InlineKeyboardButton('ទីតាំងរបស់ពួកយើង', callback_data='location')
+    service_button = InlineKeyboardButton('សេវាកម្ម', callback_data='service')
+    contact_button = InlineKeyboardButton('លេខទំនាក់ទំនង', callback_data='contact')
+    about_button = InlineKeyboardButton('អំពីយើង', callback_data='about')
+    location_button = InlineKeyboardButton('ទីតាំងរបស់ពួកយើង', callback_data='location')
+    connect_button = InlineKeyboardButton('ភ្ចាប់ជាមួយសារស្វ័យប្រវត្តិ', callback_data='connect')
+    disconnect_button = InlineKeyboardButton('ផ្តាច់ចេញពីសារស្វ័យប្រវត្តិ', callback_data='disconnect')
 
-# make above btn into a column of two buttons
-connect = InlineKeyboardButton('ភ្ចាប់ជាមួយសារស្វ័យប្រវត្តិ', callback_data='connect')
-logout = InlineKeyboardButton('ចាកចេញពីសារស្វ័យប្រវត្តិ់', callback_data='logout')
+    keyboard.row(service_button, contact_button)
+    keyboard.row(about_button, location_button)
+    if check_user_connect(chat_id) == 'false':
+        keyboard.row(connect_button)
+    else:
+        keyboard.row(disconnect_button)
 
-connect_keyboard.add(service, contact, about, location, connect)
-disconnect_keyboard.add(service, contact, about, location, logout)
-# back button from connect with hospital to main menu
-                                           
-back_button = InlineKeyboardMarkup()
-back_button.add(InlineKeyboardButton('ត្រលប់ក្រោយ', callback_data='back'))
+    return keyboard
 
-@bot.message_handler(commands=['start']) 
+def create_back_keyboard():
+    back_button = InlineKeyboardMarkup()
+    back_button.add(InlineKeyboardButton('ត្រលប់ក្រោយ', callback_data='back'))
+    return back_button
+
+@bot.message_handler(commands=['start'])
 def welcome_msg(message):
-    bot.send_message(message.chat.id, "🌟 សូមស្វាគមន៍មកកាន់ មន្ទីរពេទ្យកុមារសកល សារស្វ័យប្រវត្តិ របស់យើងនៅលើ Telegram! 🤖", reply_markup=check_user_connect(message.chat.id))
+    bot.send_message(message.chat.id, "🌟 សូមស្វាគមន៍មកកាន់ មន្ទីរពេទ្យកុមារសកល សារស្វ័យប្រវត្តិ របស់យើងនៅលើ Telegram! 🤖", reply_markup=create_main_keyboard(message.chat.id))
 
-# funcution to handle the callback data from the button for Connect with Hospital is wait user input serect code to verify user and user can cancel back to main menu
 @bot.callback_query_handler(func=lambda call: True)
 def callback_query(call):
     try:
         chat_id = call.message.chat.id
         msg_id = call.message.message_id
-        if call.data == 'logout':
-            disconnect_user_from_api(chat_id)
-    
-        elif call.data == 'connect':
+        if call.data == 'connect':
             bot.send_message(call.message.chat.id, "ភ្ចាប់ជាមួយសារស្វ័យប្រវត្តិលោកអ្នកនឹងទទួលបានការជូនដំណឹងពីពួកយើងដូចខាងក្រោម៖ \n 1. ទទួលបានសាររាល់ការណាត់ \n2. អ្នកនឹងទទួលបានដំណឹងផ្សេងៗទៀត")
             bot.send_message(call.message.chat.id, "ដើម្បីទទួលបានលេខសម្ងាត់សូមទៅកាន់កន្លែងទទួលភ្ញៀវ")
-            bot.send_message(call.message.chat.id, "សូមបញ្ជូនលេខសម្ងាត់:", reply_markup=back_button)
+            bot.send_message(call.message.chat.id, "សូមបញ្ជូនលេខសម្ងាត់:", reply_markup=create_back_keyboard())
             bot.register_next_step_handler(call.message, send_data_to_api)
+        elif call.data == 'disconnect':
+            disconnect_user(chat_id)
         elif call.data == 'service':
             get_data_from_api(chat_id,msg_id,'service')
         elif call.data == 'contact':
             get_data_from_api(chat_id,msg_id,'contact')
-
         elif call.data == 'about':
             get_data_from_api(chat_id,msg_id,'about')
         elif call.data == 'location':
             get_data_from_api(chat_id,msg_id,'location')
         elif call.data == 'back':
-            bot.edit_message_text(chat_id=chat_id, message_id=msg_id, text="🌟 សូមស្វាគមន៍មកកាន់ មន្ទីរពេទ្យកុមារសកល សារស្វ័យប្រវត្តិ របស់យើងនៅលើ Telegram! 🤖", reply_markup=check_user_connect(chat_id))
+            bot.edit_message_text(chat_id=chat_id, message_id=msg_id, text="🌟 សូមស្វាគមន៍មកកាន់ មន្ទីរពេទ្យកុមារសកល សារស្វ័យប្រវត្តិ របស់យើងនៅលើ Telegram! 🤖", reply_markup=create_main_keyboard(chat_id))
     except Exception as e:
         print(repr(e))
-        bot.send_message(call.message.chat.id, "សូមស្វាគមន៍មកកាន់ មន្ទីរពេទ្យកុមារសកល សារស្វ័យប្រវត្តិ របស់យើងនៅលើ Telegram!", reply_markup=check_user_connect(chat_id))
+        bot.send_message(call.message.chat.id, "សូមស្វាគមន៍មកកាន់ មន្ទីរពេទ្យកុមារសកល សារស្វ័យប្រវត្តិ របស់យើងនៅលើ Telegram!", reply_markup=create_main_keyboard(chat_id))
 
-# function to check user connect or not 
-def check_user_connect(chat_id):
-    url = 'http://localhost:8069/api/checkUser'
+# function to disconnect user
+def disconnect_user(chat_id):
+    url = f'{URL}/api/disconnectTelegram'
     data = {
         "jsonrpc": "2.0",
         "params": {
-            "chat_id": chat_id,
+            'chat_id': chat_id
         }
     }
     headers = {
         'Content-Type': 'application/json'
     }
+
     try:
         response = requests.post(url, headers=headers, json=data)
         if response.status_code == 200:
             result = response.json().get('result')
+            print(result)
             if result == 'true':
-                keyboard = disconnect_keyboard
+                bot.send_message(chat_id=chat_id, text="អ្នកបានផ្តាច់ចេញពីសារស្វ័យប្រវត្តិរបស់យើងហើយ!")
+                # resend welcome message with button again 
+                bot.send_message(chat_id=chat_id, text="🌟 សូមស្វាគមន៍មកកាន់ មន្ទីរពេទ្យកុមារសកល សារស្វ័យប្រវត្តិ របស់យើងនៅលើ Telegram! 🤖", reply_markup=create_main_keyboard(chat_id))
             else:
-                keyboard = connect_keyboard
-            return keyboard
+                bot.send_message(chat_id=chat_id, text="សូមព្យាយាមម្តងទៀត.", reply_markup=create_back_keyboard())
         else:
-            return connect_keyboard
-    except requests.RequestException as e:
-        return f"Request failed: {e}"
+            bot.send_message(chat_id=chat_id, text=f"Failed to send data to Odoo. Status code: {response.status_code}", reply_markup=create_back_keyboard())
+        
 
+    except requests.RequestException as e:
+        bot.send_message(chat_id=chat_id, text=f"Request failed: {e}")
 # function to send data to api
 def send_data_to_api(message):
     secret_code = message.text
     chat_id = message.chat.id
-    url = 'http://localhost:8069/api/getChatID'
+    url = f'{URL}/api/getChatID'
     data = {
         "jsonrpc": "2.0",
         "params": {
@@ -110,24 +117,44 @@ def send_data_to_api(message):
             result = response.json().get('result')
             print(result)
             if result == 'true':
-                bot.send_message(chat_id=chat_id, text="លេខសម្ងាត់ត្រូវបានផ្ទៀងផ្ទាត់ត្រឹមត្រូវ!")
-                # resend welcome message with button again 
-                bot.send_message(chat_id=chat_id, text="🌟 សូមស្វាគមន៍មកកាន់ មន្ទីរពេទ្យកុមារសកល សារស្វ័យប្រវត្តិ របស់យើងនៅលើ Telegram! 🤖", reply_markup=check_user_connect(chat_id))
+                bot.send_message(chat_id=chat_id, text="លេខសម្ងាត់ត្រូវបានផ្ទៀងផ្ទាត់ត្រឹមត្រូវ!",reply_markup=create_main_keyboard(chat_id))
             else:
-                bot.send_message(chat_id=chat_id, text="លេខសម្ងាត់មិនត្រឹមត្រូវ. សូម​ព្យាយាម​ម្តង​ទៀត.", reply_markup=back_button)
+                bot.send_message(chat_id=chat_id, text="លេខសម្ងាត់មិនត្រឹមត្រូវ. សូម​ព្យាយាម​ម្តង​ទៀត.", reply_markup=create_back_keyboard())
         else:
-            bot.send_message(chat_id=chat_id, text=f"Failed to send data to Odoo. Status code: {response.status_code}", reply_markup=back_button)
+            bot.send_message(chat_id=chat_id, text=f"Failed to send data to Odoo. Status code: {response.status_code}", reply_markup=create_back_keyboard())
         
 
     except requests.RequestException as e:
         bot.send_message(chat_id=chat_id, text=f"Request failed: {e}")
 
-# function to disconnect user from api
+# function to check user connect or not 
+def check_user_connect(chat_id):
+    url = f'{URL}/api/checkUser'
+    data = {
+        "jsonrpc": "2.0",
+        "params": {
+            'chat_id': chat_id
+        }
+    }
+    headers = {
+        'Content-Type': 'application/json'
+    }
+
+    try:
+        response = requests.post(url, headers=headers, json=data)
+        if response.status_code == 200:
+            result = response.json().get('result')
+            return result
+        else:
+            return f"Failed to get data from Odoo. Status code: {response.status_code}"
+    except requests.RequestException as e:
+        return f"Request failed: {e}"
+
 
 
 # function to get data from api
 def get_data_from_api(chat_id,msg_id,model):
-    url = 'http://localhost:8069/api/getContent'
+    url = f'{URL}/api/getContent'
     data = {
         "jsonrpc": "2.0",
         "params": {
@@ -144,7 +171,7 @@ def get_data_from_api(chat_id,msg_id,model):
             # split result with '\n' to make new line
             msg = result.replace('"','').replace('\\n','\n')
 
-            bot.edit_message_text(chat_id=chat_id, message_id=msg_id, text=msg, reply_markup=back_button)
+            bot.edit_message_text(chat_id=chat_id, message_id=msg_id, text=msg, reply_markup=create_back_keyboard())
             return result
         else:
             return f"Failed to get data from Odoo. Status code: {response.status_code}"
