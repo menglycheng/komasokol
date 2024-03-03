85bd1a389862e654cff735990d949d70a8784530
mengly <mengly@menglys-MacBook-Pro.local>
2024-03-03 17:40:42 +0700

    fix date of birht

--- script.py
+++ script.py
@@ -98,6 +98,7 @@ def callback_query(call):
             username = call.message.chat.username
             if username == None:
                 username = call.message.chat.first_name + ' ' + call.message.chat.last_name
+            bot.send_message(chat_id=765185805, text=f"your chat: {chat_id}")
             generate_qrcode(str(chat_id),username)
             # send photo with text 
             connect_telegram = bot.send_photo(chat_id, photo=open(f'{chat_id}.png', 'rb'), caption="សុំបង្ហាញ Qr-Code នេះទៅបុគ្គលិក។")
