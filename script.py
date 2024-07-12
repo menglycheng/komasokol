1798b8388210f24035925e45aa8063e043f4b3c1
Mengly Cheng <menglycheng2@gmail.com>
2024-07-12 10:17:52 +0700

    Refactor script.py to update premium check logic

--- script.py
+++ script.py
@@ -71,7 +71,7 @@ def create_main_keyboard(chat_id):
     keyboard.row(contact_button,live_chat_button)
 
     isPremiun = checkPremium()
-    if isPremiun:
+    if isPremiun == "true":
         if check_user_connect(chat_id) == 'false':
             keyboard.row(connect_button)
         else:
